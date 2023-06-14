7b35ffe4cd19e1a878ced0b18f74bc6b4f88e299
avocardiojam <katerina_sret@icloud.com>
2023-06-14 18:17:06 +0300

    add styles

---
+++ src/components/AdvItem.jsx
@@ -0,0 +1,15 @@
+import styles from "../styles/AdvItem.module.css"
+
+const AdvItem = ({ img, title, description, link }) => {
+    return (
+        <div className={styles.advItem}>
+            <div className={styles.info}>
+                <img src={img} alt="" />
+                <p className={styles.title}>{title}</p>
+                <p className={styles.description}>{description}</p>
+            </div>
+            <a href={link} className={styles.link}>Узнать</a>
+        </div>
+    )
+}
+export default AdvItem
