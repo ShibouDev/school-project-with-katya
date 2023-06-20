56234f53feb7a245c103d8437aabdcd8d75a61cf
avocardiojam <katerina_sret@icloud.com>
2023-06-20 16:44:02 +0300

    fixed FAQ

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -11,12 +11,12 @@ const FAQ = () => {
                 {FAQData.map((el, index) => (
                     <div className={styles.faqitem} key={index} onClick={() => {open === el.id ? setOpen() : setOpen(el.id)}}>
                         <div className={styles.faqitemContent}>
-                            <p className={styles.faqitemTitle}>{el.question}</p>
+                            <p className={styles.faqitemTitle}> {el.question}</p>
                             <img src={Arrow} alt="arrow" className={open === el.id ? `${styles.faqitemSvg} ${styles.arrowReverse}` : styles.faqitemSvg} />
                         </div>
                         {open === el.id &&
                             <div className={styles.faqitemOpened}>
-                                {el.answer}
+                               {el.answer}
                             </div>
                         }
                     </div>
